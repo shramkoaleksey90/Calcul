9bf0609df6d6a4ed1bea898796be71f13085fd95
h1tman <shramkoaleksey90@gmail.com>
2019-05-04 07:23:11 +0300

    memory implementation

--- src/main/java/com/dimonandpumba/calculator/Storage.java
+++ src/main/java/com/dimonandpumba/calculator/Storage.java
@@ -1,7 +1,40 @@
 package com.dimonandpumba.calculator;
 
+import java.io.*;
+import java.util.Queue;
+
+import java.util.LinkedList;
+
 public class Storage {
     public Storage(double value) {
-        System.out.println(value);
+        Queue queue = new LinkedList();
+        if (new File("memory.dat").exists()) {
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("memory.dat"));
+                queue = (Queue) objectInputStream.readObject();
+                System.out.println(queue.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("input exception");
+            }
+        }
+
+        queue.add(value);
+        if(queue.size()>5){
+            queue.poll();
+        }
+
+        for (Object object : queue ){
+            System.out.println("Value : "+object);
+        }
+
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("memory.dat"));
+            objectOutputStream.writeObject(queue);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("output");
+        }
+
     }
 }
--- src/main/java/com/dimonandpumba/calculator/View.java
+++ src/main/java/com/dimonandpumba/calculator/View.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 class View {
     private double value;
 
-    public double getValue() {
+    double getValue() {
         return value;
     }
 
